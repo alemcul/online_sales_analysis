5027f8110fde6bff8e018e313a5ec37d1c850f41
alemcul <alemcul@icloud.com>
2025-02-10 20:39:28 +0200

    Modificări în main.py înainte de merge

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from product_manager import ProductManager
 from product import Product
+from cart import Cart
 
-# Creăm instanța ProductManager
+# Creăm instanța managerului de produse
 manager = ProductManager()
 
 # Adăugăm produse
-manager.add_product(Product("Laptop", 3500, 5))
-manager.add_product(Product("Telefon", 2000, 10))
-manager.add_product(Product("Căști", 150, 20))
+manager.add_product(Product("Ultrabook", 4000, 4))
+manager.add_product(Product("Smartphone", 2200, 8))
+manager.add_product(Product("Căști wireless", 200, 15))
 
-# Afișăm produsele și valoarea totală a inventarului
-manager.display_products()
-print(manager.total_inventory_value())
-# Testăm eliminarea unui produs
-manager.remove_product("Telefon")
+# Creăm instanța coșului de cumpărături
+cart = Cart()
 
-# Afișăm din nou lista de produse pentru a verifica eliminarea
-manager.display_products()
+# Adăugăm produse în coș
+cart.add_to_cart(manager.products[0], 1)
+cart.add_to_cart(manager.products[1], 2)
+cart.add_to_cart(manager.products[2], 1)
+
+# Afișăm coșul și totalul de plată
+cart.display_cart()
+print(cart.calculate_total())
